394fb30f7eb9c6519d6096d5cc0e1c04028fb1f4
YuMoua <153407360+YuMoua@users.noreply.github.com>
2024-03-03 08:59:02 -0600

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the Title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Please enter the description of your project, the installation guide, usage information, contrabution guidelines, and test instructions.',
+    },
+    {
+        type: 'checkbox',
+        message: 'What type of licensing do you need?',
+        name: 'license',
+        choices: ['MIT License', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'githubAccount',
+        message: 'What is your github account name?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
+// put questions inside array
+
+        
+    // ])
+    // .then((data) => {
+    //     const filename = `${data.projectTitle.toLowerCase().split(' ').join('')}.json`;
+
+    //     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    //         err ? console.log(err) : console.log('Success!')
+    //     );
+    // });
+
+
+// function fetchJSONData() {
+//     fetch('./' + `${data.projectTitle.toLowerCase().split(' ').join('')}.json` + '.json');
+// };
+
+
+// function writeReadMe() {
+//     const readMeData = [projectTitle, projectDescription, liscense, githubAccount, email];
+//     fs.writeToFile(README.md, readMeData,)
+// };
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeReadMe({projectTitle, ProjectDescription, license, githubAccount, email}) {
+
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(writeReadMe())
+}
+
+
+
 
 // Function call to initialize app
 init();
+
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = 'https://opensource.org/licenses/MIT';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
